8e3dd8eaf0d7e6f047f1df8270384eb7ccc6c9f6
cfernandez <Ycuervo.659>
2024-06-24 07:31:26 -0400

    drop some technical and business exception

--- src/main/java/io/samancore/common/error/message/TechnicalExceptionsEnum.java
+++ src/main/java/io/samancore/common/error/message/TechnicalExceptionsEnum.java
@@ -10,18 +10,9 @@ public enum TechnicalExceptionsEnum implements Exceptions {
     SCHEMA_CREATE_STATEMENT(50001, "Schema Create Statement"),
     REPOSITORY_ERROR(50002, "Repository Error"),
     TRANSFORMER_OBJECT_ERROR(50003, "Transformer Object error"),
-    STRING_HANDLING_ERROR(50004, "String Handling Error"),
-    CREATE_USER_ERROR(50005, "Could not create user in keycloack"),
-    ADD_ROLE_TO_USER_ERROR(50006, "Could not add role to user in keycloack"),
-    LOAD_USER_ERROR(50007, "Could not load the user from keycloack"),
-    HTTP_CLIENT_STATUS_CODE_ERROR(50008, "Response status code received is not the expected"),
-    HCAPTCHA_COULD_NOT_CONNECT_ERROR(50009, "The response was incorrect in request to hCaptcha server"),
-    HCAPTCHA_ERROR_RESPONSE(50010, "Errors present in request to hcaptcha server"),
-    HCAPTCHA_PROCESS_ERROR(50011, "Error processing request to hcaptcha server"),
-    PROGRAM_INVALID(50012, "Program not exist"),
-    INVALID_REQUEST(50013, "Error processing the request, has invalid io.samancore.data"),
-    INVALID_STATUS_ACTION(50014, "Error processing excutor, has invalid action to handle"),
-    PROCESSING_CONDITION_ERROR(50015, "Error processing excutor, has invalid action to handle");
+    INVALID_REQUEST(50004, "Error processing the request, has invalid io.samancore.data"),
+    INVALID_STATUS_ACTION(50005, "Error processing excutor, has invalid action to handle"),
+    PROCESSING_CONDITION_ERROR(50006, "Error processing excutor, has invalid action to handle");
 
 
     private final int value;
